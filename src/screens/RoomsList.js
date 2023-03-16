50767ddf70c05e600620196031ec563b4bd0913f
brunoPierca <pierca18@gmail.com>
2023-03-16 18:25:12 -0300

    Added scrollView

--- src/screens/RoomsList.js
+++ src/screens/RoomsList.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Box, NativeBaseProvider } from "native-base";
+import { Box, NativeBaseProvider, ScrollView } from "native-base";
 import { ROOMS } from "../data/rooms";
 import { RoomItem } from "../components/RoomItem";
 
 const RoomsList = ({ route, navigation }) => {
 	return (
 		<NativeBaseProvider>
-			<Box alignItems='center'>
-				{ROOMS.map((room) => {
-					return <RoomItem key={room.id} name={room.name} description={room.description} />;
-				})}
-			</Box>
+			<ScrollView>
+				<Box alignItems='center'>
+					{ROOMS.map((room) => {
+						return <RoomItem key={room.id} name={room.name} description={room.description} />;
+					})}
+				</Box>
+			</ScrollView>
 		</NativeBaseProvider>
 	);
 };
